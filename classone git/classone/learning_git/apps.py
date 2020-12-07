cd9ea0d0962857e6202056a38e3a5cec2169923a
manjari23 <55528555+manjari23@users.noreply.github.com>
2020-12-07 11:33:28 +0530

    'first'

---
+++ classone git/classone/learning_git/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LearningGitConfig(AppConfig):
+    name = 'learning_git'
---
+++ classone git/classone/learning_git/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def day_1(request):
+    return render(request,'day_1.html')
